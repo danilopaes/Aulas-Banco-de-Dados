e12cece84527b7e2504e35e0a7172ea2c077e1de
danilopaes <danilopaesribeiro@gmail.com>
2022-07-11 21:26:14 -0300

    aulas de banco de dados

---
+++ aula.sql
@@ -0,0 +1,10 @@
+create table gravadora(
+    id serial not null,
+    nome varchar(60) null,
+    endereco varchar(60) null,
+    telefone varchar(20) null,
+    contato varchar(20) null,
+    URL varchar(80) null,
+    primary key (id)
+);
+
